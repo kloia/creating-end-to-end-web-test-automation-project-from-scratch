967826cc589654a2b8f7b7d9e302fa0ac76c97c7
muhammet_topcu <muhammet.topcu@kloia.com>
2023-06-09 15:03:36 +0200

    video_recording branch is created and configured.

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,12 +2,15 @@ require 'capybara'
 require 'capybara/dsl'
 require 'selenium-webdriver'
 require 'rspec'
+require 'screen-recorder'
 require_relative '../../utils/driver.rb'
 require_relative '../../utils/page_helper.rb'
 require_relative '../../config/base_config.rb'
 
 include Capybara::DSL
 include RSpec::Matchers
+
+ScreenRecorder.ffmpeg_binary = '/opt/homebrew/bin/ffmpeg'
 
 Capybara.configure do |config|
   config.default_driver = :selenium
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,18 +1,26 @@
-Before do
+Before do |scenario|
+  scenario_name = scenario.name.gsub(/[^A-Za-z0-9 ]/, "").gsub(/\s+/, "_")
   Driver.get_driver
   page.driver.browser.manage.window.maximize
+  puts "STARTED ==> #{scenario_name}"
+  @recorder = ScreenRecorder::Desktop.new(output: "output/failed_tests/#{scenario_name}.mkv")
+  @recorder.start
 end
 
 After do |scenario|
   begin
-    if scenario.failed?
-      puts "FAILED ==> #{scenario.name}\n#{scenario.exception}:#{scenario.exception.message}"
-    else
-      puts "PASSED ==> #{scenario.name}"
-    end
+    scenario_name = scenario.name.gsub(/[^A-Za-z0-9 ]/, "").gsub(/\s+/, "_")
     Capybara.current_session.driver.quit
+    @recorder.stop
+    if scenario.failed?
+      puts "FAILED ==> #{scenario_name}\n#{scenario.exception}:#{scenario.exception.message}"
+    else
+      puts "PASSED ==> #{scenario_name}"
+      File.delete("outputs/failed_tests/#{scenario_name}.mkv")
+    end
   rescue Exception => exception
     puts "FAILED ==> #{exception}"
     Capybara.current_session.driver.quit
+    @recorder.stop
   end
 end
