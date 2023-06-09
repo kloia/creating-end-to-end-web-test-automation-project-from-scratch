7b56ff519a8d9ceeb43b0d8e6cc8936f4c7b5249
muhammet_topcu <muhammet.topcu@kloia.com>
2023-06-09 10:03:52 +0200

    readme.md has been changed.

--- config/base_config.rb
+++ config/base_config.rb
@@ -8,6 +8,7 @@ module BaseConfig
     @wait_time
   end
 
+  #@options Inside a docker container => 'http://selenium-hub:4444'
   @remote_url = ENV['remote_url'] || 'http://localhost:4444'
   def self.remote_url
     @remote_url
